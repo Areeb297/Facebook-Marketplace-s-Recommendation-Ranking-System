198451ee2800f7a41b64db32addcd7abba6e080c
Areeb Shafqat <51030860+Areeb297@users.noreply.github.com>
2022-07-22 18:26:47 +0300

    Update merge_data.py

--- merge_data.py
+++ merge_data.py
@@ -1,9 +1,19 @@
 import pandas as pd
 from clean_tabular_data import get_data_pipeline
 
-products_data = get_data_pipeline()
-images_data = pd.read_csv("images.csv").iloc[:, 1:]
+def merge_data():
+    """This function helps in merging the product and image datasets where every product can have more than one images
 
-df = pd.merge(left=products_data, right=images_data, on=('product_id', 'create_time') , sort=True)
-df.drop(columns=['product_id', 'url', 'create_time', 'id', 'bucket_link', 'image_ref', 'page_id'], inplace=True)
+    Returns:
+        pd.DataFrame: We return a cleaned dataframe ready to use for image classification
+    
+    """
+
+    products_data = get_data_pipeline()
+    images_data = pd.read_csv("images.csv").iloc[:, 1:]
+
+    df = pd.merge(left=products_data, right=images_data, on=('product_id', 'create_time') , sort=True)
+    df.drop(columns=['product_id', 'url', 'create_time', 'id', 'bucket_link', 'image_ref', 'page_id'], inplace=True)
+
+    return df
 
