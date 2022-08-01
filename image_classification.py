cae15bbd09b05dba3ec352d686ffb6fa7d18791d
Areeb Shafqat <51030860+Areeb297@users.noreply.github.com>
2022-08-01 18:59:46 +0300

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -12,60 +12,57 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 
-def images_to_array(path):
-    """This function converts all the image files in cleaned_images to np.array format, reshapes them into 2D array,
-    stores them as a list, converts the data into a pandas DataFrame and saves the dataframe as a pickle file
+
+def category_encode_decode(dataframe):
+    """This function returns the decoding of product categories after using cat.codes
 
     Args:
-        path (str): The path to where the cleaned images are kept
-    Returns:
-        pd.DataFrame: The dataframe with images as arrays
+        dataframe (pd.DataFrame): Merged dataframe of products and image information
+
+    Returns
+        dict: Decoded dictionary for product categories
+        pd.DataFrame: Updated dataframe with encoded categories
     """
-    # check first if pickle file already exists
-    if not os.path.exists('image_dataframe.pkl'):
-        arr_im_list = []
-        dirs = os.listdir(path)
-
-        for item in dirs:
-            im = Image.open(path + item)
-            numpydata = np.asarray(im) # convert image to array
-            arr_im_list.append(numpydata)
-
-        featueres = pd.DataFrame(np.reshape(arr_im_list, (12600, 64*64*3))) # (64*64*3) are the sizes we saved the images as 
-            # Store data (serialize)
-        with open('image_dataframe.pkl', 'wb') as file:
-            pickle.dump(featueres, file)
-    else:   
-        file = open("image_dataframe.pkl",'rb')
-        featueres = pickle.load(file)
     
-    return featueres
+    dataframe.category = dataframe.category.astype('category')
+    dataframe['category_codes'] = dataframe.category.cat.codes
+
+    return dict(enumerate(dataframe['category'].cat.categories)), dataframe
 
 def image_classification():
     """This function gets the product and image tabular data merged together, converts the category into numerical codes,
     takes the category as target, splits the data into traning and testing and use logistic regression to predict the 
     product category based on the image data, printing the accuracy score and classification report
     """
+    if not os.path.exists('image_dataframe.pkl'):
+        merge_data.merge_im_array()
 
-    df = merge_data.merge()
-    df.sort_values(by='id', inplace=True) # So that the order of the images in both the tabular and non-tabular are the same
-    df.category = df.category.apply(lambda x: x.split('/')[0])
-    df.category = df.category.astype('category')
-    df['category_codes'] = df.category.cat.codes
+    file = open("image_dataframe.pkl",'rb')
+    df = pickle.load(file)
+
+    df.category = df.category.apply(lambda x: x.split('/')[0]) # Retain only the first category
+
+    decode, df = category_encode_decode(df)
+    with open('decoder.pkl', 'wb') as file:
+        pickle.dump(decode, file)
 
     y = df.category_codes # target variable
     with open('image_targets.pkl', 'wb') as file:
         pickle.dump(y, file)
 
-    X = images_to_array('cleaned_images/')
+    X = df['image_array'].apply(lambda x: x.flatten())
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    with open('features.pkl', 'wb') as file:
+        pickle.dump(X, file)
+
+    X_train, X_test, y_train, y_test = train_test_split(list(X), y, test_size=0.3, random_state=42)
 
     param_grid = [    
-    {'penalty' : ['l2', 'none'],
-    'C' : np.logspace(-4, 4, 20),
-    'solver' : ['newton-cg','lbfgs', 'sag','saga'], # For multi-classification
-    'max_iter' : [300, 1000, 1500],
+    {'penalty' : ['l2'],
+    'C' : np.logspace(-4, 4, 30),
+    'solver' : ['lbfgs'], # For multi-classification (newton-cg, sag, saga)
+    'max_iter' : [300, 600, 1200],
+
     }
     ]
 
@@ -74,9 +71,15 @@ def image_classification():
     random_search = sklearn.model_selection.RandomizedSearchCV(
         estimator=model, 
         param_distributions=param_grid,
-        verbose=4, n_iter=3, cv=2
+        verbose=4, n_iter=2, cv=3
         
         )
+
+
+# For small datasets, liblinear is a good choice, whereas sag and saga are faster for large ones;
+# For multiclass problems, only newton-cg, sag, saga and lbfgs handle multinomial loss;
+# liblinear is limited to one-versus-rest schemes.
+
     
     random_search.fit(X_train, y_train)
     y_pred = random_search.predict(X_test)
@@ -84,7 +87,7 @@ def image_classification():
     print(random_search.best_params_)
     print(f'The accuracy of our predictions: {round(accuracy_score(y_test, y_pred), 5) * 100} %')
     print(classification_report(y_test, y_pred))
-    print(dict(enumerate(df['category'].cat.categories))) # Prints which code corresponds to which category
+    print(decode) # Prints which code corresponds to which category
 
 if __name__ == '__main__':
     image_classification()
