a0f5a0dfd56bbd197599bcbf534ac5f420bba06d
Areeb Shafqat <51030860+Areeb297@users.noreply.github.com>
2022-08-06 02:51:48 +0300

    Update basic_CNN_classification.py

--- basic_CNN_classification.py
+++ basic_CNN_classification.py
@@ -85,12 +85,12 @@ class CNN(torch.nn.Module):
         # Declaring the Architecture
         self.layers = torch.nn.Sequential(
             torch.nn.Conv2d(3, 200, 5, 2), # Kernel size 7 with stride 2
-            torch.nn.MaxPool2d(2,2),
+            torch.nn.MaxPool2d(4,4),
             torch.nn.Dropout(p=0.1),
             torch.nn.ReLU(),
             
             torch.nn.Conv2d(200, 100, 3),
-            torch.nn.MaxPool2d(2, 2), # Max pooling (2, 2) filter
+            # torch.nn.MaxPool2d(2, 2), # Max pooling (2, 2) filter
             torch.nn.Dropout(p=0.1),
             torch.nn.ReLU(),
 
@@ -100,7 +100,8 @@ class CNN(torch.nn.Module):
             torch.nn.ReLU(),
 
             torch.nn.Flatten(),
-            torch.nn.Linear(3200 , 13), #  Predicting 13 product categories
+            torch.nn.Linear(2450, 100),
+            torch.nn.Linear(100, 13), #  Predicting 13 product categories
             torch.nn.Softmax(dim=1)
         )
 
