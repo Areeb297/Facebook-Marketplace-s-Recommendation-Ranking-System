fb130e7ac3eb23c8b749f459207f6d4fd45da09e
Areeb Shafqat <51030860+Areeb297@users.noreply.github.com>
2022-08-09 19:52:48 +0300

    Update and rename Regression.py to regression.py

--- regression.py
+++ regression.py
@@ -1,4 +1,4 @@
-import clean_tabular_data
+from clean_tabular_data import get_data_pipeline
 from nltk.corpus import stopwords
 import numpy as np
 
@@ -20,7 +20,7 @@ def regression_func():
     
     """
     # We get the cleaned product tabular data from the clean_tabular_data python script
-    product_data = clean_tabular_data.get_data_pipeline()
+    product_data = get_data_pipeline()
     # Features
     X = product_data[['product_name', 'product_description', 'location']]
     y = product_data['price']
@@ -33,7 +33,11 @@ def regression_func():
     [
         ("vector_1", TfidfVectorizer(stop_words=stop), 'product_name'),
         ("vector_2", TfidfVectorizer(stop_words=stop), 'product_description'),
-        ("vector_3", TfidfVectorizer(), 'location')], remainder='passthrough') # We don't need stopwords for location
+        ("vector_3", TfidfVectorizer(stop_words=stop), 'location')
+        
+        ], 
+        
+        remainder='passthrough') 
 
 
 
@@ -50,7 +54,7 @@ def regression_func():
     # set parameters for the tfidf vectors
     parameters = {
         'tfidf__vector_1__ngram_range': ((1, 1), (1, 2), (1, 3)),
-        'tfidf__vector_2__min_df': (0.0001,  0.005),
+        'tfidf__vector_2__min_df': (0.0001,  0.005, 0.01),
 
         'tfidf__vector_2__ngram_range': ((1, 1),(1, 2)),
         'tfidf__vector_1__min_df': (0.0005,  0.001)
@@ -58,10 +62,8 @@ def regression_func():
 
     # Find the best hyperparameters for both the feature extraction and regressor
     grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=2)
-
-
     # split data in to train/test
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     grid_search.fit(X_train, y_train)
 
@@ -74,3 +76,12 @@ def regression_func():
 
 if __name__ == '__main__':
     regression_func()
+
+# Results:
+# Best params:
+#{'tfidf__vector_1__min_df': 0.0005, 'tfidf__vector_1__ngram_range': (1, 1), 
+#'tfidf__vector_2__min_df': 0.0001, 'tfidf__vector_2__ngram_range': (1, 2)}
+# RMSE: 86118.08025602228
+# The r^2 score was: -57.482586087335896
+
+
