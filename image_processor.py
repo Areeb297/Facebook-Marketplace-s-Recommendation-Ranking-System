ff32b08808d997a10827de3352d4aae3c660fd3e
Areeb Shafqat <51030860+Areeb297@users.noreply.github.com>
2022-08-09 21:07:49 +0300

    Add files via upload

---
+++ image_processor.py
@@ -0,0 +1,31 @@
+from torchvision import transforms
+# import os
+# from PIL import Image
+
+class ImageProcessor():
+    def __init__(self):
+        self.transform = transforms.Compose([
+        transforms.Resize(128), # change from 155 to 128
+        transforms.CenterCrop(128),
+        transforms.RandomHorizontalFlip(p=0.3),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225]) 
+                    ])
+
+    def __call__(self, image): 
+        if image.mode != 'RGB':
+            image = image.convert('RGB')
+        image = self.transform(image)
+        image = image[None, :, :, :]
+        image = image.reshape(1, 3, 128, 128)
+        return image
+                
+
+# if __name__ == '__main__':
+#     img_dir = os.listdir('cleaned_images')
+#     img = img_dir[3]
+#     img = Image.open('cleaned_images/'+ img)
+#     process = ImageProcessor()
+#     img_new = process(img)
+#     print(img_new.shape)
